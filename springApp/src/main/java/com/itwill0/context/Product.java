b4cab5459c94dbcc19b62f6f6bdd980a65478626
growworld <growworld@naver.com>
2020-02-20 18:02:22 +0900

    Spring intro

---
+++ springApp/src/main/java/com/itwill0/context/Product.java
@@ -0,0 +1,7 @@
+package com.itwill0.context;
+
+public class Product {
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ springApp/src/main/java/com/itwill0/context/ProductService.java
@@ -0,0 +1,18 @@
+package com.itwill0.context;
+
+import java.util.List;
+
+public class ProductService {
+	private ProductDao productDao;
+	public ProductService() {
+		System.out.println("1.### ProductService()생성자");
+	}
+	public void setProductDao(ProductDao productDao) {
+		System.out.println("2.### ProductService.seProductDao("+productDao+")");
+		this.productDao = productDao;
+	}
+	public List<Product> productList(){
+		return productDao.productList();
+	}
+
+}
